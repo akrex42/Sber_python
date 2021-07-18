76aee7dfca6842390ec8948a04119ee2ba97a30b
polya <polina.s.nekrasova@gmail.com>
2021-07-18 19:17:12 +0300

    unit 8 added all done

--- unit_8_task_3.py
+++ unit_8_task_3.py
@@ -1,39 +1,56 @@
 s = input()
 s1 = '!@#$%^&*()-+'
-l = 0
-u = 0
-d = 0
+low = "добавить 1 строчную букву,"
+u = "добавить 1 заглавную букву,"
+d = "добавить 1 цифру,"
 i = 0
+sp = "добавить 1 спецсимвол,"
+er = 0
+length = 'увеличить число символов -'
+var = 0
 
-while i < len:
-    if s[i].isdigit():
-        d += 1
-    if s[i].islower():
-        l += 1
-    if s[i].isupper():
-        u += 1
-    i += 1
-
-i = 0
-j = 0
 while i < len(s):
-    while j < len(s1):
-        if s[i] == s1[j]:
-            s += 1
-        j += 1
+    if s[i].isdigit():
+        d = ''
+    elif s[i].islower():
+        low = ''
+    elif s[i].isupper():
+        u = ''
+    else:
+        j = 0
+        while j < len(s1):
+            if s[i] == s1[j]:
+                er = 2
+                break
+            j += 1
+        if er == 2:
+            sp = ''
+            er = 0
+        else:
+            er = 1
     i += 1
-if s != 0 and d != 0 and u != 0 and l != 0 and len(s) >= 12:
-    printu('Сильный пароль.\n')
-else:
-    printu('Слабый пароль. Рекомендации: ')
+s2 = ''
 if len(s) < 12:
     var = 12 - len(s)
-    printu('увеличить число символов - ', var)
-if u == 0:
-    printu("добавить 1 заглавную букву ")
-if d == 0:
-    printu("добавить 1 цифру ")
-if l == 0:
-    printu("добавить 1 строчную букву ")
-if s == 0:
-    printu("добавить 1 спецсимвол ")
+    length = length + ' ' + str(var) + ','
+else:
+    length = ''
+if er == 1:
+    print('Ошибка. Запрещенный спецсимвол')
+elif sp == '' and d == '' and u == '' and low == '' and len(s) >= 12:
+    print('Сильный пароль.')
+else:
+    print('Слабый пароль. Рекомендации: ', end='')
+    if var:
+        s1 = length + u + low + d + sp
+    else:
+        s1 = u + low + d + sp
+    myList = s1.split(',')
+    myList1 = []
+    for word in myList:
+        if word != '':
+            myList1.append(word)
+    print(', '.join(myList1), end='')
+
+
+
