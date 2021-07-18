8f686b89e1204648281d9c8d3c6bb89f94630359
polya <polina.s.nekrasova@gmail.com>
2021-07-18 23:26:55 +0300

    task 3 unit 9 done

--- unit_9/task_3.py
+++ unit_9/task_3.py
@@ -1,6 +1,5 @@
-str_list = input("Введите список:\n")
+str_list = input("Enter the list:\n")
 list1 = str_list.split(', ')
-list2 = []
 i = 0
 j = 0
 k = 0
@@ -14,4 +13,11 @@ while i < len(list1):
         maxim = int(list1[i])
         k = i
     i += 1
-print(list2)
+list1.pop(j)
+list1.insert(j, str(maxim))
+list1.pop(k)
+list1.insert(k, str(minim))
+
+# could use remove instead
+
+print(list1)
