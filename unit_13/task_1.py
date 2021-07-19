4217e057991f67ef819837ec42ef8eb9b9b13106
polya <polina.s.nekrasova@gmail.com>
2021-07-19 18:51:37 +0300

    unit_13 done

---
+++ unit_13/task_1.py
@@ -0,0 +1,52 @@
+def do_arithmetic_operation(string_args):
+    ''' do_arithmetic_operation function takes a string, converts it to a list,
+        then parse a list adn do  the proper operation
+        :param string_args:
+        :return: result of arithmetic operation
+    '''
+    my_list = list(string_args)
+    my_str = ''
+    flag_second_digit = 0
+    sign = ''
+    for item in my_list:
+        if item.isdigit():
+            my_str += str(item)
+        else:
+            break
+    first_digit = int(my_str)
+    for item in my_list:
+        if item.isdigit():
+            continue
+        else:
+            sign += str(item)
+    my_str = ''
+    i = 0
+    while i < len(my_list):
+        if not my_list[i].isdigit():
+            while i < len(my_list):
+                if my_list[i].isdigit():
+                    my_str += str(my_list[i])
+                    flag_second_digit = 1
+                i += 1
+            break
+        i += 1
+    if flag_second_digit:
+        second_digit = int(my_str)
+        if sign == '+':
+            return first_digit + second_digit
+        elif sign == '-':
+            return first_digit - second_digit
+        elif sign == '/':
+            return first_digit / second_digit
+        elif sign == '*':
+            return first_digit * second_digit
+        elif sign == '**':
+            return first_digit ** second_digit
+    else:
+        if sign == '%':
+            return first_digit / 100
+        elif sign == '**':
+            return first_digit ** 2
+
+
+print(do_arithmetic_operation(input()))
