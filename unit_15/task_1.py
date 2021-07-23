db66d18a6e697a5c553b695d9b04cddb84e667a6
polya <polina.s.nekrasova@gmail.com>
2021-07-23 15:40:46 +0300

    unit_15 done but without functions

--- unit_15/task_1.py
+++ unit_15/task_1.py
@@ -7,50 +7,55 @@ def email_gen(list_of_names):
         emails.append(i[1] + "." + i[0][0:letter] + '@company.io')
     return emails
 
-list_of_names = []
-list1 = []
-flag = 0
-file = open("task_file.txt", 'r+w')
-# file1 = open("result.txt", 'r+w')
-# lines = file1.readlines()
-# file1.write(str(file.readlines()[:1]))
-# file.close()
-# file = open("task_file.txt", 'r+w')
-for line in file:
-    list1 = line.split(',')
-    flag = 0
-    # here I should create a cycle to check all the blanks
-    for item in list1[1:len(list1)]:
-        if list1[1][1:1] == '':
-            line = ''
-            break
-        elif list1[2][1:1] == '':
-            line = ''
-            break
-        elif len(list1[len(list1) - 2]) != 8:
-            line = ''
-            break
-        elif flag == 0:
-            list1.append(list1[1][1:len(list1[1])])
-            list1.append(list1[2][1:len(list1[2])])
-            list_of_names.append(list1)
-            list1 = []
-            flag = 1
-emails = []
-i = 0
-print(list_of_names)
-emails = email_gen(list_of_names)
-print(emails)
 
-file.close()
-file = open("task_file.txt", 'r+w')
+list_of_names_gen = []
+emails_raw = []
+list1 = []
+list2 = []
+flag_blank = 0
+file = open("task_file.txt", 'r+')
 for line in file:
     list1 = line.split(',')
-    print(list1)
-    # list[0] = emails[i]
-    # i+=1
-# file.close()
-# file = open("task_file.txt", 'r+')
-# for line in file:
-#     print(line)
-# file1.write(line)
+    if len(list1[3][1:]) != 7 or not list1[3][1:].isdigit():
+        continue
+    for it in list1[1:]:
+        if it == ' ':
+            flag_blank = 1
+            break
+    if not flag_blank:
+        list2.append(list1[1][1:])
+        list2.append(list1[2][1:])
+        list_of_names_gen.append(list2)
+        list2 = []
+    elif flag_blank:
+        flag_blank = 0
+emails_raw = email_gen(list_of_names_gen)
+file.seek(0)
+j = 0
+index = 0
+flag_blank = 0
+list_of_lines = file.readlines()
+for line in list_of_lines:
+    list1 = line.split(',')
+    if len(list1[3][1:]) != 7 or not list1[3][1:].isdigit():
+        list_of_lines[j] = line
+        j += 1
+        continue
+    for it in list1[1:]:
+        if it == ' ':
+            flag_blank = 1
+            break
+    if not flag_blank:
+        if list1[0] == '':
+            list_of_lines[j] = str(emails_raw[index]) + line
+            index += 1
+    elif flag_blank:
+        flag_blank = 0
+        list_of_lines[j] = line
+    j += 1
+file.close()
+file = open("task_file.txt", 'w+')
+file.writelines(list_of_lines)
+file.seek(0)
+for line in file:
+    print(line)
