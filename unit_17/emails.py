5a087b9158cd11b2d7cd9e373c3b9277bc9592bd
polya <polina.s.nekrasova@gmail.com>
2021-07-25 15:25:01 +0300

    final project works, but I need to use more regular expressions

--- unit_17/emails.py
+++ unit_17/emails.py
@@ -11,46 +11,20 @@ def email_gen(list_of_names):
 def fill_emails():
     list_of_names_gen = []
     list2 = []
-    flag_blank = 0
     file = open("file_sample.txt", 'r+')
     for line in file:
         list1 = line.split(',')
-        if len(list1[3][1:]) != 7 or not list1[3][1:].isdigit():
-            continue
-        for it in list1[1:]:
-            if it == ' ':
-                flag_blank = 1
-                break
-        if not flag_blank:
-            list2.append(list1[1][1:])
-            list2.append(list1[2][1:])
-            list_of_names_gen.append(list2)
-            list2 = []
-        elif flag_blank:
-            flag_blank = 0
+        list2.append(list1[1][1:])
+        list2.append(list1[2][1:])
+        list_of_names_gen.append(list2)
+        list2 = []
     emails_raw = email_gen(list_of_names_gen)
     file.seek(0)
     j = 0
-    index = 0
-    flag_blank = 0
     list_of_lines = file.readlines()
     for line in list_of_lines:
-        list1 = line.split(',')
-        if len(list1[3][1:]) != 7 or not list1[3][1:].isdigit():
-            list_of_lines[j] = line
-            j += 1
-            continue
-        for it in list1[1:]:
-            if it == ' ':
-                flag_blank = 1
-                break
-        if not flag_blank:
-            if list1[0] == '':
-                list_of_lines[j] = str(emails_raw[index]) + line
-                index += 1
-        elif flag_blank:
-            flag_blank = 0
-            list_of_lines[j] = line
+        if list_of_lines[j].split(',')[0] == '':
+            list_of_lines[j] = emails_raw[j] + list_of_lines[j]
         j += 1
     file.close()
     file = open("file_sample.txt", 'w+')
--- unit_17/final_project.py
+++ unit_17/final_project.py
@@ -3,15 +3,35 @@ from emails import *
 import re
 
 file = open("file_sample.txt", 'r+')
+file1 = open("file_errors.txt", 'a+')
 list_of_lines = file.readlines()
-i = 0
-if re.search(r'PASSWORDS', list_of_lines[0]).group(0) != 'PASSWORDS':
+i = 1
+flag_blank = 0
+if re.search(r'PASSWORDS', list_of_lines[0]) is None:
     list_of_lines[0] = list_of_lines[0].replace('\n', ', ') + 'PASSWORDS' + '\n'
+file.seek(0)
+for line in list_of_lines[1:]:
+    list1 = line.split(',')
+    if len(list1[3][1:]) != 7 or not list1[3][1:].isdigit():
+        file1.write(list_of_lines[i])
+        list_of_lines.pop(i)
+        continue
+    for it in list1[1:]:
+        if it == ' ':
+            file1.write(list_of_lines[i])
+            list_of_lines.pop(i)
+            flag_blank = 1
+            break
+    if not flag_blank and len(list_of_lines[i].split(',')) == 5:
+        list_of_lines[i] = list_of_lines[i][:len(list_of_lines[i]) - 1] + ', ' + password_gen(20) + '\n'
+        i += 1
+    elif flag_blank:
+        flag_blank = 0
 file.close()
 file = open("file_sample.txt", 'w+')
 file.writelines(list_of_lines)
-file.seek(0)
+file.close()
+fill_emails()
+file = open("file_sample.txt", 'r+')
 for line in file:
     print(line)
-# password_gen(19)
-# fill_emails()
